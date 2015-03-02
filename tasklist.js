12807731b586fd8ee4e1784bf1578632f64bc55d
Phillip Pan <phillip.y.pan@gmail.com>
2015-03-01 23:51:24 -0600

    first commit

---
+++ tasklist.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+    $("form").submit(function(event) {
+    	var text = $("input:first").val();
+    	var select = $("#myselect").val();
+    	console.log(select);
+
+        $("#"+select+"-list").append("<div class='task'><span class='del'>[x]</span><span class='"+select+"'> &#9632;</span> "+text+"</div>");
+    });
+
+    $("#high-list").on('click', 'div.task span.del', function() {
+    	console.log('something');
+    	$(this).parent().remove();
+    });
+
+    $("#medium-list").on('click', 'div.task span.del', function() {
+    	console.log('something');
+    	$(this).parent().remove();
+    });
+
+    $("#low-list").on('click', 'div.task span.del', function() {
+    	console.log('something');
+    	$(this).parent().remove();
+    });
+
+});
